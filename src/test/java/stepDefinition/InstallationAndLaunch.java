cf7e96bee70f79dfc091312e09550cb717645e67
AGiallelis <agiallelis1@gmail.com>
2025-03-02 11:12:10 +0200

    Several step definition implemented

---
+++ src/test/java/stepDefinition/InstallationAndLaunch.java
@@ -0,0 +1,58 @@
+package stepDefinition;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppSteps {
+    private static AndroidDriver driver;
+
+    @Given("Install the app from the respective app store \(Google Play for Android, App Store for iOS\)")
+    public void installAppFromStore() {
+        System.out.println("Ensure the app is installed from the respective store.");
+    }
+
+    @Then("Open the app for the first time")
+    public void openAppForFirstTime() {
+        setup();
+        System.out.println("Launching the app for the first time.");
+    }
+
+    @Given("Install the app")
+    public void installApp() {
+        System.out.println("Ensure the app is installed on the device.");
+    }
+
+    @When("Update the app through the app store")
+    public void updateApp() {
+        System.out.println("Updating the app through the store.");
+    }
+
+    @Then("Launch the app after the update")
+    public void launchAppAfterUpdate() {
+        setup();
+        System.out.println("Launching the app after updating.");
+    }
+
+    private void setup() {
+        if (driver == null) {
+            try {
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("deviceName", "emulator-5554");
+                caps.setCapability("appPackage", "com.example.app"); // Replace with actual app package
+                caps.setCapability("appActivity", "com.example.app.MainActivity"); // Replace with actual activity
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/stepDefinition/NavigationAndUIElements.java
@@ -0,0 +1,89 @@
+package stepDefinition;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppSteps {
+    private static AndroidDriver driver;
+
+    @Given("Navigate to the login page")
+    public void navigateToLoginPage() {
+        WebElement loginPage = driver.findElement(By.id("login_page_id")); // Replace with actual ID
+        loginPage.click();
+    }
+
+    @When("the user Enter \"(.*)\" in the username field")
+    public void enterUsername(String username) {
+        WebElement usernameField = driver.findElement(By.id("username_field_id")); // Replace with actual ID
+        usernameField.sendKeys(username);
+    }
+
+    @When("the user Enter \"(.*)\" in the password field")
+    public void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.id("password_field_id")); // Replace with actual ID
+        passwordField.sendKeys(password);
+    }
+
+    @Then("the user clicks the \"Login\" button")
+    public void clickLoginButton() {
+        WebElement loginButton = driver.findElement(By.id("login_button_id")); // Replace with actual ID
+        loginButton.click();
+    }
+
+    @Given("Navigate to the top-right menu")
+    public void navigateToTopRightMenu() {
+        WebElement menuButton = driver.findElement(By.id("menu_button_id")); // Replace with actual ID
+        menuButton.click();
+    }
+
+    @Then("Click the \"Logout\" button")
+    public void clickLogoutButton() {
+        WebElement logoutButton = driver.findElement(By.id("logout_button_id")); // Replace with actual ID
+        logoutButton.click();
+    }
+
+    @Given("From the home screen \(product list\), tap on the \"Cart\" icon")
+    public void tapOnCartIcon() {
+        WebElement cartIcon = driver.findElement(By.id("cart_icon_id")); // Replace with actual ID
+        cartIcon.click();
+    }
+
+    @Then("Tap on the \"Back\" button to return to the product list")
+    public void tapOnBackButton() {
+        WebElement backButton = driver.findElement(By.id("back_button_id")); // Replace with actual ID
+        backButton.click();
+    }
+
+    @Given("Open the app on a large phone or tablet")
+    public void openAppOnLargeDevice() {
+        System.out.println("Test case for responsive design on a large device.");
+    }
+
+    @Then("Navigate through the product list and other screens")
+    public void navigateThroughScreens() {
+        System.out.println("Navigating through product list and other screens.");
+    }
+
+    private void setup() {
+        if (driver == null) {
+            try {
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("deviceName", "emulator-5554");
+                caps.setCapability("appPackage", "com.example.app"); // Replace with actual app package
+                caps.setCapability("appActivity", "com.example.app.MainActivity"); // Replace with actual activity
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
